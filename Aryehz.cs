879be94c09bddecba6be48a4633447f34a4737dd
Jay Kravetz <jayk@ltccs.com>
2016-11-15 16:44:47 -0500

    Create our first program file

---
+++ Aryehz.cs
@@ -0,0 +1,9 @@
+use System;
+
+public class SomeProgramThatOnlyRunsOnTheWindowsDotNetPlatform {
+    
+    static void Main(String[] args) 
+    {
+        System.Console.WriteLine("Hello Bob");
+    }
+}
