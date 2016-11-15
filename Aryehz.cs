99e338fd1f026edba3a5b4632aef3aa9a8a7469f
Conference Room <ConferenceRoom@ltccs.com>
2016-11-15 17:14:32 -0500

    Add a comment

--- Aryehz.cs
+++ Aryehz.cs
@@ -2,6 +2,7 @@ using System;
 
 public class SomeProgramThatOnlyRunsOnTheWindowsDotNetPlatform {
 
+    // This is a comment, also known as an apology
     static void Main(String[] args)
     {
         System.Console.WriteLine("Hello Bob");
